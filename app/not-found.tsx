773e4d715d3c2221f6d81ffe5f7713bd87915a41
Bishnu <thapabishnu20@gmail.com>
2024-01-26 12:57:19 +0545

    add not-found and error page

---
+++ app/not-found.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NotFound = () => {
+  return <div>The requested Page doesn&apos;t exists!</div>;
+};
+
+export default NotFound;
---
+++ app/users/[id]/not-found.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserNotFound = () => {
+  return <div>The requested user doesn&apos;t exists!</div>;
+};
+
+export default UserNotFound;
