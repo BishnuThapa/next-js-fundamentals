1f9d96d3fb36389f7bb4b8055d4a825a9c9c0fef
Bishnu <thapabishnu20@gmail.com>
2024-01-30 19:57:51 +0545

    access sessions on the client & show login username in navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,7 +1,10 @@
+"use client";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
+  const { status, data: session } = useSession();
   return (
     <>
       <div className="flex w-full bg-slate-900 text-white gap-5 p-5 py-4">
@@ -17,9 +20,15 @@ const Navbar = () => {
         <Link className="mr-5" href="/admin">
           Admin
         </Link>
-        <Link className="mr-5" href="/api/auth/signin">
-          Sign in
-        </Link>
+        {status === "loading" && <span>Loading...</span>}
+        {status === "unauthenticated" && (
+          <Link className="mr-5" href="/api/auth/signin">
+            Sign in
+          </Link>
+        )}
+        {status === "authenticated" && (
+          <span className="mr-5">{session.user?.email}</span>
+        )}
       </div>
     </>
   );
